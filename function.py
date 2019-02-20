ccdf06acdd9bc53b6aaec935e355a8b9fa3c25b3
JesseLazier <xbhw0126@hotmail.com>
2019-02-21 02:18:06 +0800

    funtion练习

---
+++ function.py
@@ -0,0 +1,29 @@
+#
+#
+# #define 定义
+# def func1():
+#     print("hello function")
+#
+# func1()
+
+#
+# a=10
+# b=10
+#
+# print(a+b)
+#
+# c=20
+# d=20
+# print(c+d)
+#
+# def sum(num1,num2):
+#     print(num1+num2)
+#
+# sum(1,2)
+# sum(10,10)
+
+def fun2(a,b):
+    return (a+b)
+
+b=fun2(1,3)
+print(b)
