ae89f90020e5d6b6744aa115736d8029c30b7e43
JesseLazier <xbhw0126@hotmail.com>
2019-02-21 00:10:20 +0800

    while loop练习

---
+++ while_loop.py
@@ -0,0 +1,14 @@
+
+condition=1
+while condition<=5:
+    print(condition)
+    condition=condition+1
+
+for i in range(0,5):
+    print(i+1)
+
+while True:
+    a = input("please enter number a: ")
+    b = input("please enter number b: ")
+
+    print("a+b="+(a+b))
