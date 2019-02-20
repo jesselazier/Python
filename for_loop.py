9626c17caf3634eb0772084afec8172bff9cdf6a
JesseLazier <xbhw0126@hotmail.com>
2019-02-20 23:47:35 +0800

    for loop 练习

---
+++ for_loop.py
@@ -0,0 +1,15 @@
+
+
+
+my_list=["jake","Tom","hong wei"]
+
+
+for i in my_list:
+    print(i)
+
+#print 1-10
+for number in range(0,10):
+    print(number+1)
+
+for letter in 'jesse':
+    print("each letter is: "+letter)
