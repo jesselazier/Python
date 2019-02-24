dddddf47e2810187ca53cc3988eac3d17fdc0607
JesseLazier <xbhw0126@hotmail.com>
2019-02-24 16:15:31 +0800

    class练习

---
+++ ClassPractice.py
@@ -0,0 +1,18 @@
+
+
+
+#object oriented
+
+
+class Student():
+    def eat(self):
+        print("student can eat")
+    def study(self):
+        print("student can study")
+
+
+Student().eat()
+
+
+friut="apple"
+print(friut.upper())
