a9f61d61b4022c25068d28666dc113db6d8477df
JesseLazier <xbhw0126@hotmail.com>
2019-02-25 23:16:21 +0800

    加载网络上的方法 使用方法模块

---
+++ Modules.py
@@ -0,0 +1,16 @@
+
+
+import random
+from math import pi as pai  #方法pi重命名为pai，利用as关键
+from selenium import webdriver #自动化需要载入selenuim
+
+webdriver.chrome
+
+
+print(pai)
+
+
+for i in range(5):
+    value=random.randint(1,6)
+    print(value)
+
