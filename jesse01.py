765168df6ffcfa817660b0344df74663aeb7260c
JesseLazier <xbhw0126@hotmail.com>
2019-02-26 00:32:46 +0800

    if __name__=='__main__'

---
+++ jesse01.py
@@ -0,0 +1,8 @@
+#if __name__=='__main__'练习
+
+def m1():
+    print('this is from Jesse01 module')
+
+#必须用if __name__=='__main__'把内容围起来
+if __name__=='__main__':
+    m1()
---
+++ jesse02.py
@@ -0,0 +1,8 @@
+#if __name__=='__main__'练习
+#防止调用jesse01
+
+def m2():
+    print('this is jesse02 module')
+
+m2()
+#jesse01.m1()
